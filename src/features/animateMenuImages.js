3c0508890a573752d0f5663965b7b5356a37c1de
net11FellaJ <julian.fella@neteleven.de>
2023-12-11 14:30:44 +0100

    animate menu images

---
+++ src/features/animateMenuImages.js
@@ -0,0 +1,24 @@
+import hoverEffect from 'hover-effect'
+
+const animateMenuImages = () => {
+  const navigationFlyoutWrap = document.querySelector('.navigation-flyout')
+  const activeNavigationFlyoutImage = navigationFlyoutWrap.querySelector(
+    '.navigation_content-item-image-wrap-inner.is-active img'
+  ).src
+  const activeNavigationFlyoutImage2 = document.querySelector(
+    '.navigation_content-item-image-wrap-inner img'
+  ).src
+  console.log(activeNavigationFlyoutImage)
+  console.log(activeNavigationFlyoutImage2)
+
+  new hoverEffect({
+    parent: navigationFlyoutWrap,
+    intensity: 0.3,
+    image1: activeNavigationFlyoutImage,
+    image2: activeNavigationFlyoutImage2,
+    displacementImage:
+      'https://uploads-ssl.webflow.com/6564c4456080e0bdcc61f7b1/65708ab9d60d4ae70c2cf469_displacement-4.png',
+  })
+}
+
+export default animateMenuImages
