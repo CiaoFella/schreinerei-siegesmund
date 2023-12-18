de433a7cc2cebdea99106eee6ec6da3a3191fa2f
net11FellaJ <julian.fella@neteleven.de>
2023-12-18 13:37:23 +0100

    finalize animate Teaser animation

--- src/features/animateTeaser.js
+++ src/features/animateTeaser.js
@@ -1,7 +1,6 @@
 import gsap from 'gsap'
 import ScrollToPlugin from 'gsap/ScrollToPlugin'
 import ScrollTrigger from 'gsap/src/ScrollTrigger'
-// import SplitType from 'split-type'
 
 gsap.registerPlugin(ScrollToPlugin)
 gsap.registerPlugin(ScrollTrigger)
@@ -29,12 +28,6 @@ function animateTeaser() {
     '.strenghts-column-inner.is-reversed'
   )
 
-  // const imgWrap = teaserWrap.querySelector('.strenghts-img-wrap.is-middle')
-  // const textWrap = imgWrap.querySelectorAll('.heading-style-h2')
-  // const button = imgWrap.querySelectorAll('.button-icon')
-  // const allChildren = [textWrap, button]
-  // const splitText = new SplitType(textWrap)
-
   const teaserTl = gsap.timeline({
     scrollTrigger: {
       trigger: teaserWrap,
@@ -42,16 +35,9 @@ function animateTeaser() {
       end: 'bottom bottom',
       scrub: 0.5,
     },
-    onComplete: () => {
-      console.log('finished')
-    },
   })
 
-  teaserTl.fromTo(
-    headline,
-    { opacity: 1, y: 0 },
-    { opacity: 0, y: '100%', ease: 'expo.out' }
-  )
+  teaserTl.fromTo(headline, { y: 0 }, { y: '100%', ease: 'expo.out' })
   teaserTl.fromTo(
     allColumns,
     { width: '100vw', height: '350%' },
@@ -69,6 +55,7 @@ function animateTeaser() {
   )
   teaserTl.fromTo(centerImages, { scale: 1.5 }, { scale: 1, delay: 0.2 }, '<')
   teaserTl.fromTo(teaserSection, { opacity: 1 }, { opacity: 0, duration: 0.2 })
+  teaserTl.fromTo(teaserSection, { autoAlpha: 1 }, { autoAlpha: 0 }, '<')
 
   function playNextSectionAnimation() {
     const nextSection = document.querySelector('.section--strenghts-after')
@@ -76,11 +63,9 @@ function animateTeaser() {
     const nextSectionTl = gsap.timeline({
       scrollTrigger: {
         trigger: nextSection,
-        start: '10% top',
-        markers: true,
+        start: '2% top',
         toggleActions: 'play play none reverse',
       },
-      onStart: console.log(teaserWrap),
     })
     nextSectionTl.fromTo(
       images,
@@ -95,38 +80,6 @@ function animateTeaser() {
   }
 
   playNextSectionAnimation()
-
-  // playTextAnimation = function textAnimation() {
-  //   const textTl = gsap.timeline({
-  //     onStart: () => {
-  //       allChildren.forEach((child) => {
-  //         child[0].classList.add('is--animated')
-  //       })
-  //     },
-  //   })
-  //   textTl.fromTo(
-  //     splitText.lines,
-  //     { y: '50%', opacity: 0 },
-  //     {
-  //       y: 0,
-  //       opacity: 1,
-  //       stagger: 0.2,
-  //       duration: 0.75,
-  //       ease: 'power2.out',
-  //     }
-  //   )
-  //   textTl.fromTo(
-  //     button,
-  //     { y: '50%', opacity: 0 },
-  //     {
-  //       y: 0,
-  //       opacity: 1,
-  //       duration: 0.5,
-  //       ease: 'power2.out',
-  //     },
-  //     '>-0.25'
-  //   )
-  // }
 }
 
 export default animateTeaser
