c3aac33b327f993971967f43189ae9359be430e3
net11FellaJ <julian.fella@neteleven.de>
2024-02-06 12:18:32 +0100

    add text to contact links

--- src/features/setInitialState.js
+++ src/features/setInitialState.js
@@ -4,5 +4,6 @@ export default function setInitialState() {
   const $allContactOpenLinks = $('[data-open=contact]')
   $allContactOpenLinks.each((index, item) => {
     $(item).css('cursor', 'pointer')
+    $(item).attr('data-cursor-text', 'Hallo!')
   })
 }
