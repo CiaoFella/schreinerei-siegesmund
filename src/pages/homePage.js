0f4ba705210a3335a345bb9d0627aaf5bc69422d
net11FellaJ <julian.fella@neteleven.de>
2024-02-17 15:14:51 +0100

    add animateHero to homePage

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -6,10 +6,12 @@ import animateTextScroll from '../features/shared/features/animateTextScroll'
 import contactPopup from '../features/shared/features/contactPopup'
 import syncCmsList from '../features/shared/features/syncCmsList'
 import animateNavbar from '../features/shared/features/animateNavbar'
+import animateHomeHero from '../features/homePage/animateHomeHero'
 
 export default function initHomePage() {
   return [
     animateNavbar(),
+    animateHomeHero.animateHero(),
     animateIntro(),
     animateTextScroll(),
     animateStrenghts(),
