fa02861d84fa94ec7b9a671b4ef93b3c0aa672bf
net11FellaJ <julian.fella@neteleven.de>
2024-02-19 16:34:30 +0100

    refactor text scroll animation

--- src/features/shared/features/animateTextScroll.js
+++ src/features/shared/features/animateTextScroll.js
@@ -3,7 +3,6 @@ let $ = window.$
 import gsap from 'gsap'
 import ScrollTrigger from 'gsap/dist/ScrollTrigger'
 import SplitType from 'split-type'
-
 import { varBlack, varGray } from '../utils/varbiables'
 
 gsap.registerPlugin(ScrollTrigger)
@@ -15,9 +14,20 @@ function animateTextScroll() {
   const $allScrollHeadlines = $('.section--scroll-headline')
 
   $allScrollHeadlines.each(function (index, item) {
+    const singleHeadlineTl = gsap.timeline()
+    headlineScrollTl.add(singleHeadlineTl)
+
+    ScrollTrigger.create({
+      animation: singleHeadlineTl,
+      trigger: item,
+      start: 'top top',
+      end: 'bottom 75%',
+      toggleActions: 'play reverse play reverse',
+    })
+
     const $allTextLines = $(item).find('.is--text-line')
     const splitWords = new SplitType($allTextLines, { types: 'words' })
-    headlineScrollTl.from(splitWords.words, {
+    singleHeadlineTl.from(splitWords.words, {
       y: '10rem',
       stagger: 0.05,
       duration: 1.25,
@@ -26,29 +36,20 @@ function animateTextScroll() {
   })
 
   $allTextScrolls.each(function (index, item) {
-    const $allTextAccent = $(item).find('span.text-accent-italic')
-    $allTextAccent.each((index, textItem) => {
-      textScrollTl.fromTo(textItem, { color: varGray }, { color: varBlack })
-    })
-  })
+    const singleScrollTl = gsap.timeline()
+    textScrollTl.add(singleScrollTl)
 
-  $allScrollHeadlines.each(function (index, item) {
     ScrollTrigger.create({
-      animation: headlineScrollTl,
-      trigger: item,
-      start: 'top top',
-      end: 'bottom 75%',
-      toggleActions: 'play reverse play reverse',
-    })
-  })
-
-  $allTextScrolls.each(function (index, item) {
-    ScrollTrigger.create({
-      animation: textScrollTl,
+      animation: singleScrollTl,
       trigger: item,
       start: 'top 25%',
       end: 'bottom 75%',
       scrub: 0.5,
+    })
+
+    const $allTextAccent = $(item).find('span.text-accent-italic')
+    $allTextAccent.each((index, textItem) => {
+      singleScrollTl.fromTo(textItem, { color: varGray }, { color: varBlack })
     })
   })
 
