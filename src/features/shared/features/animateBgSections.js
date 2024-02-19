017cd12ea3bd1443435cf6fd7f5698ad805e2e73
net11FellaJ <julian.fella@neteleven.de>
2024-02-19 16:34:11 +0100

    refactor bg section animation

--- src/features/shared/features/animateBgSections.js
+++ src/features/shared/features/animateBgSections.js
@@ -3,85 +3,50 @@ let $ = window.$
 import gsap from 'gsap'
 import ScrollTrigger from 'gsap/dist/ScrollTrigger'
 
-import { varBlack, varWhite, varTransparent } from '../utils/varbiables'
-
 gsap.registerPlugin(ScrollTrigger)
 
 function animateBgSections() {
   const backgroundTl = gsap.timeline({ defaults: { overwrite: 'auto' } })
-  let backgroundColor
-  let textColor
-  let previousBackgroundColor
-  let previousTextColor
 
   const $allBgSections = $('[data-bg]')
+  let bgClass
+  let textClass
   $allBgSections.each((index, section) => {
-    const body = $('body')
-    const sectionBackgroundData = $(section).data('bg')
-    const closestSection = $(section).closest('div[class*=section--]')
-    const targetText = closestSection.find(
-      `[data-bg-text=${sectionBackgroundData}]`
+    const $backgroundTarget = $('.section-wrapper')
+    const $sectionBackgroundData = $(section).data('bg')
+    const $closestSection = $(section).closest('div[class*=section--]')
+    const $targetText = $closestSection.find(
+      `[data-bg-text=${$sectionBackgroundData}]`
     )
 
-    if (sectionBackgroundData === 'dark') {
-      previousBackgroundColor = varTransparent
-      previousTextColor = varBlack
-      backgroundColor = varBlack
-      textColor = varWhite
+    if ($sectionBackgroundData === 'dark') {
+      bgClass = 'dark-bg'
+      textClass = 'dark-bg-text'
     }
 
     ScrollTrigger.create({
       animation: backgroundTl,
       trigger: section,
-      start: 'top 50%',
+      start: 'top bottom',
+      end: 'bottom top',
       onEnter: () => {
-        backgroundTl
-          .to(body, {
-            backgroundColor: backgroundColor,
-            duration: 0.25,
-          })
-          .to(targetText, {
-            color: textColor,
-            duration: 0.25,
-          })
+        $backgroundTarget.addClass(bgClass)
+        $targetText.addClass(textClass)
       },
       onEnterBack: () => {
-        backgroundTl
-          .to(body, {
-            backgroundColor: backgroundColor,
-            duration: 0.25,
-          })
-          .to(targetText, {
-            color: textColor,
-            duration: 0.25,
-          })
+        $backgroundTarget.addClass(bgClass)
+        $targetText.addClass(textClass)
       },
       onLeaveBack: () => {
-        backgroundTl
-          .to(body, {
-            backgroundColor: previousBackgroundColor,
-            duration: 0.25,
-          })
-          .to(targetText, {
-            color: previousTextColor,
-            duration: 0.25,
-          })
+        $backgroundTarget.removeClass(bgClass)
+        $targetText.removeClass(textClass)
       },
       onLeave: () => {
-        backgroundTl
-          .to(body, {
-            backgroundColor: previousBackgroundColor,
-            duration: 0.25,
-          })
-          .to(targetText, {
-            color: previousTextColor,
-            duration: 0.25,
-          })
+        $backgroundTarget.removeClass(bgClass)
+        $targetText.removeClass(textClass)
       },
     })
   })
-
-  return [backgroundTl]
 }
 
 export default animateBgSections
